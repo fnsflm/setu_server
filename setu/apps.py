71e8c9870587f75411dd026233ec42bfac362a4a
fnsflmzqdydk@github.com <1094159280@qq.com>
2021-02-16 21:10:41 +0800

    a django project

---
+++ setu/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SetuConfig(AppConfig):
+    name = 'setu'
---
+++ setu/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def setsetu(request):
+    return render(request, "setu.html", {
+        "setu": "setu_database/かにビーム/DzJ3-03VYAAnK54.jpg"
+    })
