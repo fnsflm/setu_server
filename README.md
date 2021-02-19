9736e581043cc79ded91d488119c3b61239de7aa
fnsflmzqdydk@github.com <1094159280@qq.com>
2021-02-19 14:39:33 +0800

    random setu

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # setu_server
 ## 随机setu
 简单的随机setu功能, 自己收集的图库(3.1G), 确保原图(VPS阿里云1M)
+
 http://setu.fnsflm.xyz:8000/random
 
-TODO LIST
+
+## TODO LIST
 (咕咕咕)
 - [x] 绑定setu.fnsflm.xyz域名到服务器上, 访问时能返回setu
 - [x] 对url增加参数, 能根据不同参数解析返回不同结果
--- setu/__init__.py
+++ setu/__init__.py
@@ -1,9 +1,7 @@
 import os
+## pics: 保存随机setu的路径
 file_root = os.walk("setu_database")
 pics = []
 for path, dir_list, file_list in file_root:
     for file_name in file_list:
-        # print(os.path.join(path,file_name))
         pics.append(os.path.join(path, file_name))
-        # print(path)
-# print(pics)
